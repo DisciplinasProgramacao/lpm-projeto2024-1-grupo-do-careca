fc08df2e90b3ca1ceb1d4eef58f8b79056390c8e
Pedro Farias Pires <125492933+pedrinhofp10@users.noreply.github.com>
2024-03-21 13:55:37 -0300

    Criação classe "Mesa"

--- codigo/Mesa.java
+++ codigo/Mesa.java
@@ -1,5 +1,21 @@
 package codigo;
 
 public class Mesa {
-    
+    public class Mesa {
+    private int quantidadeDeCadeiras;
+    private boolean mesaOcupada;
+
+    public Mesa(int quantidadeDeCadeiras) {
+        this.quantidadeDeCadeiras = quantidadeDeCadeiras;
+        this.mesaOcupada = false;
+    }
+
+    public boolean cabemNaMesa(int quantidade) { //O método cabemNaMesa(int quantidade) verifica se a quantidade de pessoas cabe na mesa e retorna um booleano.
+        return quantidade <= quantidadeDeCadeiras;
+    } 
+
+    public void ocuparMesa() { //O método ocuparMesa() marca a mesa como ocupada.
+        this.mesaOcupada = true;
+    }
+}
 }
