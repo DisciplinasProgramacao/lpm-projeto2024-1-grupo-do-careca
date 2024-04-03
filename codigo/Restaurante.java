9bc4afca18feca9daa5237adb1a5bcc9b105fd1e
Lucas Oliveira <72320508+oliveira25lucas@users.noreply.github.com>
2024-04-03 14:15:25 -0300

    Documentação do codigo

--- codigo/Restaurante.java
+++ codigo/Restaurante.java
@@ -1,15 +1,22 @@
 class Restaurante {
+
+    //Lista de mesas disponíveis no restaurante, e Fila de espera para clientes que precisam de mesas.
     private List<Mesa> mesas;
     private Queue<Cliente> filaDeEspera;
 
+    //Construtor vazio
     public Restaurante() {
         this.mesas = new ArrayList<>();
         this.filaDeEspera = new LinkedList<>();
     }
 
+    //Adiciona uma mesa ao restaurante.
     public void addMesa(Mesa mesa) {
         this.mesas.add(mesa);
     }
+
+    //Verifica se existe uma mesa disponível no restaurante com capacidade suficiente para o número de pessoas pedido.
+
 
     public boolean temMesaDisponivel(int quantidadeDePessoas) {
         for (Mesa mesa : mesas) {
@@ -19,10 +26,14 @@ class Restaurante {
         }
         return false;
     }
+     
+    //Adiciona um cliente à fila de espera.
 
     public void enviarClienteParaFilaDeEspera(Cliente cliente) {
         this.filaDeEspera.add(cliente);
     }
+
+    //Senta o cliente no início da fila de espera em uma mesa disponível.
 
     public void sentarClienteDaFilaDeEspera() {
         if (!filaDeEspera.isEmpty()) {
@@ -30,6 +41,8 @@ class Restaurante {
             alocarCliente(cliente);
         }
     }
+    
+     //Aloca um cliente em uma mesa disponível.
 
     public void alocarCliente(Cliente cliente) {
         for (Mesa mesa : mesas) {
