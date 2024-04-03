3a4e107fc4296b2a9f1745aca2d07b129224d654
Lucas Oliveira <72320508+oliveira25lucas@users.noreply.github.com>
2024-04-03 14:10:32 -0300

    Primeitro teste classe restaurante

--- codigo/Restaurante.java
+++ codigo/Restaurante.java
@@ -1,5 +1,42 @@
-package codigo;
+class Restaurante {
+    private List<Mesa> mesas;
+    private Queue<Cliente> filaDeEspera;
 
-public class Restaurante {
-    
+    public Restaurante() {
+        this.mesas = new ArrayList<>();
+        this.filaDeEspera = new LinkedList<>();
+    }
+
+    public void addMesa(Mesa mesa) {
+        this.mesas.add(mesa);
+    }
+
+    public boolean temMesaDisponivel(int quantidadeDePessoas) {
+        for (Mesa mesa : mesas) {
+            if (!mesa.isOcupada() && mesa.getCapacidade() >= quantidadeDePessoas) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void enviarClienteParaFilaDeEspera(Cliente cliente) {
+        this.filaDeEspera.add(cliente);
+    }
+
+    public void sentarClienteDaFilaDeEspera() {
+        if (!filaDeEspera.isEmpty()) {
+            Cliente cliente = filaDeEspera.poll();
+            alocarCliente(cliente);
+        }
+    }
+
+    public void alocarCliente(Cliente cliente) {
+        for (Mesa mesa : mesas) {
+            if (!mesa.isOcupada() && mesa.getCapacidade() >= cliente.getQuantidadeDePessoas()) {
+                mesa.setOcupada(true);
+                break;
+            }
+        }
+    }
 }
