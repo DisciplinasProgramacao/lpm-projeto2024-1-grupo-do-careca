a7aac086ee4fbb20ecf170a80e45ae4f071ba079
PedroL <pepluisgon01@gmail.com>
2024-04-10 16:12:48 -0300

    Teste da classe cliente basicao

---
+++ codigo/ClienteTest.java
@@ -0,0 +1,33 @@
+package codigo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ClienteTest {
+
+    @Test
+    public void testGetNome() {
+        Cliente cliente = new Cliente("João", 3);
+        assertEquals("João", cliente.getNome());
+    }
+
+    @Test
+    public void testSetNome() {
+        Cliente cliente = new Cliente("Maria", 2);
+        cliente.setNome("Ana");
+        assertEquals("Ana", cliente.getNome());
+    }
+
+    @Test
+    public void testGetQuantidadeDePessoas() {
+        Cliente cliente = new Cliente("Carlos", 5);
+        assertEquals(5, cliente.getQuantidadeDePessoas());
+    }
+
+    @Test
+    public void testCabemNaMesa() {
+        Cliente cliente = new Cliente("Fernanda", 1);
+        assertTrue(cliente.cabemNaMesa(2)); // A mesa tem mais cadeiras do que clientes
+    }
+
+}
