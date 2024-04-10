3b80e96c23d19252001173fec1f3d03983e14cbc
Pedro Farias Pires <125492933+pedrinhofp10@users.noreply.github.com>
2024-04-10 14:35:00 -0300

    correção classe mesa

--- codigo/Mesa.java
+++ codigo/Mesa.java
@@ -1,21 +1,27 @@
 package codigo;
 
 public class Mesa {
-    public class Mesa {
     private int quantidadeDeCadeiras;
     private boolean mesaOcupada;
 
     public Mesa(int quantidadeDeCadeiras) {
+        if (quantidadeDeCadeiras != 4 && quantidadeDeCadeiras != 6 && quantidadeDeCadeiras != 8) {
+            throw new IllegalArgumentException("A quantidade de cadeiras deve ser 4, 6 ou 8.");
+        }
         this.quantidadeDeCadeiras = quantidadeDeCadeiras;
         this.mesaOcupada = false;
     }
 
-    public boolean cabemNaMesa(int quantidade) { //O método cabemNaMesa "int quantidade" verifica se a quantidade de pessoas cabe na mesa e retorna um booleano.
-        return quantidade <= quantidadeDeCadeiras;
-    } 
+    public boolean cabemNaMesa(int quantidade) {
+        return !mesaOcupada && quantidade <= quantidadeDeCadeiras;
+    }
 
-    public void ocuparMesa() { //O método "ocuparMesa" marca a mesa como ocupada.
+    public void ocuparMesa() {
         this.mesaOcupada = true;
     }
+
+    public void desocuparMesa() {
+        this.mesaOcupada = false;
+    }
 }
-}
+
