562f2879619e134ab3dcfd76edfc30d320e39720
Pedro Goncalves <pepluisgon01@gmail.com>
2024-05-29 16:56:11 -0300

    Ajustando classe main

--- codigo/Main.java
+++ codigo/Main.java
@@ -18,90 +18,30 @@ public class Main {
             System.out.println("1. Atender Cliente"); //coleta de dados de um cliente e processo de pedir uma mesa
             System.out.println("2. Ver Fila de Espera");
             System.out.println("3. Servir Cliente");
-            System.out.println("4. Encerrar Atendimento de Cliente"); //processo de vagar uma mesa
-            System.out.println("5. Ver Menu");
-            System.out.println("6. Selecionar Produto");
-            System.out.println("7. Incluir Produto no Pedido");
-            System.out.println("8. Fechar Conta");
-            System.out.println("9. Mostrar Conta");
-            System.out.println("10. Sair");
+            System.out.println("4. Encerrar Atendimento de Cliente"); //fecha a conta, mostra a conta e vaga uma mesa
+            System.out.println("5. Ver Menu");   
+            System.out.println("6. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
             scanner.nextLine();
 
             switch (opcao) {
                 case 1:                
-                    atenderCliente(scanner, restaurante);
-                    break;
+                    atenderCliente(scanner, restaurante);break;                    
                 case 2:
-                    verFilaDeEspera(restaurante);
-                    break;
+                    verFilaDeEspera(restaurante);break;                    
                 case 3:
-                    servirCliente(scanner, restaurante);                    
-                    break;
-
+                    servirCliente(scanner, restaurante);break;
                 case 4:
-                //ecerrarAtendimento
-                    System.out.println("Mesas Ocupadas:");
-                    List<Mesa> mesasOcupadas = restaurante.getMesasOcupadas();
-                    if (mesasOcupadas.isEmpty()) {
-                        System.out.println("Nenhuma mesa ocupada.");
-                    } else {
-                        for (int i = 0; i < mesasOcupadas.size(); i++) {
-                            Mesa mesaOcupada = mesasOcupadas.get(i);
-                            Requisicao req = mesaOcupada.getRequisicaoAtual();
-                            System.out.println((i + 1) + ". Mesa para " + mesaOcupada.getQuantidadeDeCadeiras()
-                                    + " pessoas ocupada por " + req.getCliente().getNome());
-                        }
-                        System.out.print("Escolha uma mesa para liberar (1-" + mesasOcupadas.size() + "): ");
-                        int indiceMesa = scanner.nextInt();
-                        scanner.nextLine();
-                        if (indiceMesa > 0 && indiceMesa <= mesasOcupadas.size()) {
-                            Mesa mesaEscolhida = mesasOcupadas.get(indiceMesa - 1);
-                            Requisicao req = mesaEscolhida.getRequisicaoAtual();
-                            if (req != null) {
-                                restaurante.liberarMesa(mesaEscolhida);
-                                System.out.println("Mesa liberada com sucesso!");
-                                System.out.println("Relatório de Atendimento:");
-                                System.out.println(req.relatorioAtendimento());
-                                List<Pedido> pedidos = req.getPedidos();
-                                for (Pedido p : pedidos) {
-                                    System.out.println("Pedido:");
-                                    for (Item item : p.getItemsEscolhidos()) {
-                                        System.out.println(" - " + item.getNome() + ", Preço: R$" + item.getPreco());
-                                    }
-                                    System.out.println("Menu fechado: " + p.isMenuFechado());
-                                }
-                            } else {
-                                System.out.println("Nenhuma requisição encontrada para esta mesa.");
-                            }
-                        } else {
-                            System.out.println("Opção inválida.");
-                        }
-                    }
-                    break;
-                    case 5:
-                    verMenu();
-                    break;
+                    encerrarAtendimento(scanner, restaurante);break;
+                case 5:
+                    verMenu(cardapio);break;                    
                 case 6:
-                    //selecionarProduto()
-                    break;
-                case 7:
-                    //incluirProduto()
-                    break;
-                case 8:
-                    //fecharConta( )
-                    break;
-                case 9:
-                    //mostrarConta()
-                    break;
-                case 10:
-                    System.out.println("Saindo...");
-                    break;
+                System.out.println("Saindo...");break;     
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
-        } while (opcao != 10);
+        } while (opcao != 6);
 
         scanner.close();
     }
@@ -110,25 +50,7 @@ public class Main {
     //Permitir ver menu, selecionar produto, incluir produto, fechar conta, mostrar conta
 
 
-    private static void servirCliente(Scanner scanner, Restaurante restaurante) {
-        System.out.print("Escolha uma mesa para servir o cliente: ");
-        int numeroMesa = scanner.nextInt();
-        scanner.nextLine();
-
-        if (!restaurante.mesaExiste(numeroMesa)) {
-            System.out.println("Mesa não existe.");
-            return;
-        }
-
-        if (!restaurante.verificarMesaOcupada(numeroMesa)) {
-            System.out.println("A mesa escolhida não está ocupada.");
-            return;
-        }
-
-        criarPedido(scanner, restaurante, numeroMesa);
-    }
-    
-    
+     //opcao 1
     private static void atenderCliente(Scanner scanner, Restaurante restaurante){
         System.out.print("Nome do Cliente: ");
                     String nome = scanner.nextLine();
@@ -150,6 +72,8 @@ public class Main {
                     }
                     
     }
+
+    //opcao 2
     private static void verFilaDeEspera(Restaurante restaurante) {
         System.out.println("Fila de Espera:");
                     Queue<Requisicao> filaEspera = restaurante.getFilaEspera();
@@ -162,22 +86,29 @@ public class Main {
                         }
                     }
     }
-    private static void encerrarAtendimento() {}
-    private static void verMenu() {}
-    private static void selecionarProduto() {}
-    private static void incluirProduto() {}
-    private static void fecharConta() {}
-    private static void monstrarConta() {}
-    
-    private static Pedido obterPedidoExistente(Mesa mesa) {
-        Pedido pedido = mesa.getPedido();
-        if (pedido == null) {
-            pedido = new Pedido(false);
-            mesa.setPedido(pedido);
+
+
+    //opcao 3
+    private static void servirCliente(Scanner scanner, Restaurante restaurante) {
+        System.out.print("Escolha uma mesa para servir o cliente: ");
+        int numeroMesa = scanner.nextInt();
+        scanner.nextLine();
+
+        if (!restaurante.mesaExiste(numeroMesa)) {
+            System.out.println("Mesa não existe.");
+            return;
         }
-        return pedido;
+
+        if (!restaurante.verificarMesaOcupada(numeroMesa)) {
+            System.out.println("A mesa escolhida não está ocupada.");
+            return;
+        }
+
+        criarPedido(scanner, restaurante, numeroMesa);
     }
-    
+   
+   
+
     private static void criarPedido(Scanner scanner, Restaurante restaurante, int numeroMesa) {
         // Valida se ta tem peido
         Mesa mesa = restaurante.encontrarMesaPorNumero(numeroMesa);
@@ -220,7 +151,65 @@ public class Main {
     
         System.out.println("Pedido realizado com sucesso.");
     }
+
+    //opcao 4
+    private static void encerrarAtendimento(Scanner scanner, Restaurante restaurante) {
+        System.out.println("Mesas Ocupadas:");
+        List<Mesa> mesasOcupadas = restaurante.getMesasOcupadas();
+        if (mesasOcupadas.isEmpty()) {
+            System.out.println("Nenhuma mesa ocupada.");
+        } else {
+            for (int i = 0; i < mesasOcupadas.size(); i++) {
+                Mesa mesaOcupada = mesasOcupadas.get(i);
+                Requisicao req = mesaOcupada.getRequisicaoAtual();
+                System.out.println((i + 1) + ". Mesa para " + mesaOcupada.getQuantidadeDeCadeiras() + " pessoas ocupada por " + req.getCliente().getNome());
+            }
+            System.out.print("Escolha uma mesa para liberar (1-" + mesasOcupadas.size() + "): ");
+            int indiceMesa = scanner.nextInt();
+            scanner.nextLine();
+            if (indiceMesa > 0 && indiceMesa <= mesasOcupadas.size()) {
+                Mesa mesaEscolhida = mesasOcupadas.get(indiceMesa - 1);
+                liberarMesaComRelatorio(restaurante, mesaEscolhida);
+            } else {
+                System.out.println("Opção inválida.");
+            }
+        }
+    }
+
+    //opcao 5
+    private static void verMenu(Cardapio cardapio) {
+        List<Item> itensCardapio = cardapio.getItems();
+        for (Item item : itensCardapio) {
+            System.out.println(item.getIdentificador() + ". " + item.getNome() + " - R$ " + item.getPreco());
+        }
+    }        
+
+    private static void liberarMesaComRelatorio(Restaurante restaurante, Mesa mesaEscolhida) {
+        Requisicao req = mesaEscolhida.getRequisicaoAtual();
+        if (req != null) {
+            restaurante.liberarMesa(mesaEscolhida);
+            System.out.println("Mesa liberada com sucesso!");
+            System.out.println("Relatório de Atendimento:");
+            System.out.println(req.relatorioAtendimento());
+            mostrarConta(req);
+        } else {
+            System.out.println("Nenhuma requisição encontrada para esta mesa.");
+        }
+    }
+
+    private static void mostrarConta(Requisicao req) {
+        List<Pedido> pedidos = req.getPedidos();
+        for (Pedido p : pedidos) {
+            System.out.println("Pedido:");
+            for (Item item : p.getItemsEscolhidos()) {
+                System.out.println(" - " + item.getNome() + ", Preço: R$" + item.getPreco());
+            }
+            System.out.println("Menu fechado: " + p.isMenuFechado());
+        }
+    }
     
-    
+
+
+   
 
 }
