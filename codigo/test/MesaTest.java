41c57f9b53ab6627fe21f9893a7f97a1af399f0a
Lucas Oliveira <72320508+oliveira25lucas@users.noreply.github.com>
2024-06-26 08:21:04 -0300

    Correção MesaTest.java

--- codigo/test/MesaTest.java
+++ codigo/test/MesaTest.java
@@ -1,67 +1,52 @@
-package codigo.test;
+package codigo.entities;
 
-import org.junit.Test;
-
-import codigo.entities.Cliente;
-import codigo.entities.Mesa;
-import codigo.entities.Pedido;
-import codigo.entities.Requisicao;
-
-import static org.junit.Assert.*;
-
-import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MesaTest {
 
-    private Mesa mesa;
+    @Test
+    public void testMesaConstructor() {
+        Mesa mesa1 = new Mesa(4);
+        assertEquals(1, mesa1.getIdMesa());
+        assertEquals(4, mesa1.getQuantidadeDeCadeiras());
+        assertFalse(mesa1.isMesaOcupada());
+        assertNull(mesa1.getPedido());
 
-    @Before
-    public void setUp() {
-        mesa = new Mesa(4);
+        Mesa mesa2 = new Mesa(6);
+        assertEquals(2, mesa2.getIdMesa());
+        assertEquals(6, mesa2.getQuantidadeDeCadeiras());
+        assertFalse(mesa2.isMesaOcupada());
+        assertNull(mesa2.getPedido());
     }
 
     @Test
-    public void testMesaInicializacao() {
-        assertEquals(4, mesa.getQuantidadeDeCadeiras());
-        assertFalse(mesa.isMesaOcupada());
-        assertNull(mesa.getRequisicaoAtual());
-        assertNull(mesa.getPedido());
+    public void testSetQuantidadeDeCadeiras() {
+        Mesa mesa = new Mesa(4);
+        mesa.setQuantidadeDeCadeiras(5);
+        assertEquals(5, mesa.getQuantidadeDeCadeiras());
     }
 
     @Test
-    public void testOcuparDesocuparMesa() {
+    public void testOcuparMesa() {
+        Mesa mesa = new Mesa(4);
         mesa.ocuparMesa();
         assertTrue(mesa.isMesaOcupada());
+    }
 
+    @Test
+    public void testDesocuparMesa() {
+        Mesa mesa = new Mesa(4);
+        mesa.ocuparMesa();
         mesa.desocuparMesa();
         assertFalse(mesa.isMesaOcupada());
     }
 
     @Test
-    public void testSetQuantidadeDeCadeiras() {
-        mesa.setQuantidadeDeCadeiras(6);
-        assertEquals(6, mesa.getQuantidadeDeCadeiras());
-    }
-
-    @Test
-    public void testSetRequisicaoAtual() {
-        Cliente cliente = new Cliente("Xulambs");
-        Requisicao requisicao = new Requisicao(4, cliente);
-        mesa.setRequisicaoAtual(requisicao);
-        assertEquals(requisicao, mesa.getRequisicaoAtual());
-    }
-
-    @Test
     public void testSetPedido() {
-        Pedido pedido = new Pedido(false);
+        Mesa mesa = new Mesa(4);
+        Pedido pedido = new Pedido();
         mesa.setPedido(pedido);
         assertEquals(pedido, mesa.getPedido());
-    }
-
-    @Test
-    public void testGetIdMesa() {
-        int id = mesa.getIdMesa();
-        Mesa novaMesa = new Mesa(2);
-        assertNotEquals(id, novaMesa.getIdMesa());
     }
 }
