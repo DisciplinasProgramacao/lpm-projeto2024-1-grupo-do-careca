e6a0355d5557de36cece303a7f77681eec8a9010
Lucas Oliveira <72320508+oliveira25lucas@users.noreply.github.com>
2024-06-26 08:15:36 -0300

    Correção ItemTest.java

--- codigo/test/ItemTest.java
+++ codigo/test/ItemTest.java
@@ -1,41 +1,39 @@
-package codigo.test;
+package codigo.entities;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import codigo.entities.Item;
+public class ItemTest {
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-
-public class ItemTest { 
-
-    private Item item;
-
-    @Before
-    public void setUp() {
-        item = new Item("Falafel Assado", 15.0, 1);
+    @Test
+    public void testItemConstructor() {
+        Item item = new Item("Moqueca de palmito", 32.00, 1);
+        assertEquals("Moqueca de palmito", item.getNome());
+        assertEquals(32.00, item.getPreco());
+        assertEquals(1, item.getIdentificador());
     }
 
     @Test
     public void testGetNome() {
+        Item item = new Item("Falafel Assado", 20.00, 2);
         assertEquals("Falafel Assado", item.getNome());
     }
 
-   
-
     @Test
     public void testGetPreco() {
-        assertEquals(15.0, item.getPreco(),0.1);
+        Item item = new Item("Salada primavera com macarrão Konjac", 25.00, 3);
+        assertEquals(25.00, item.getPreco());
     }
-
-  
 
     @Test
     public void testGetIdentificador() {
-        assertEquals(1, item.getIdentificador());
+        Item item = new Item("Escondidinho de Inhame", 18.00, 4);
+        assertEquals(4, item.getIdentificador());
     }
 
-   
-    
+    @Test
+    public void testToString() {
+        Item item = new Item("Strogonoff de cogumelos", 35.00, 5);
+        assertEquals("Item: Strogonoff de cogumelos, Preço: R$35.0", item.toString());
+    }
 }
